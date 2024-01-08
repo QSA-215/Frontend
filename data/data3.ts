5df2b2b450d2ffd1e56a40b3c973d9b55621afb9
QSA-215 <dolgorukov-22122003@mail.ru>
2024-01-08 21:51:28 +0300

    Final part

--- data/data3.ts
+++ data/data3.ts
@@ -1,108 +1,5 @@
 import * as types from "./types";
 
-const ThirdHistory: types.History = {
-	next: [],
-	previous: [
-		{
-			size: {
-				width: 700,
-				height: 700
-			},
-			background: {
-				backgroundType: 'img',
-				url: '../images/flowers.png'
-			},
-			filter: 'none',
-			objects: [
-				{
-					id: 1,
-					position: {
-						x: 300,
-						y: 0 
-					},
-					type: {
-						objectType: 'text',
-						str: 'КОООТ',
-						fontSize: 50,
-						color: '#f0f0f0',
-						bold: false,
-						italic: true,
-						underline: true
-					},
-					selectionFlag: false
-				},
-				{
-					id: 2,
-					position: {
-						x: 150,
-						y: 100
-					},
-					type: {
-						objectType: 'img',
-						url: '../images/cat.png',
-						size: {
-							width: 300,
-							height: 200
-						}
-					},
-					selectionFlag: false
-				},
-				{
-					id: 3,
-					position: {
-						x: 200,
-						y: 120 
-					},
-					type: {
-						objectType: 'figure',
-						type: 'circle',
-						size: {
-							width: 100,
-							height: 100
-						},
-						color: '#ffffff'
-					},
-					selectionFlag: false
-				},
-				{
-					id: 4,
-					position: {
-						x: 150,
-						y: 100
-					},
-					type: {
-						objectType: 'figure',
-						type: 'rectangle',
-						size: {
-							width: 300,
-							height: 200
-						},
-						color: "#ffff00"
-					},
-					selectionFlag: false
-				},
-				{
-					id: 5,
-					position: {
-						x: 200,
-						y: 200
-					},
-					type: {
-						objectType: 'figure',
-						type: 'triangle',
-						size: {
-							width: 100,
-							height: 150
-						},
-						color: "#00ff00"
-					},
-					selectionFlag: false
-				}
-				]
-		}
-		]
-};
-
 const ThirdCanvas: types.CanvasModel = {
 			size: {
 				width: 700,
@@ -112,13 +9,63 @@ const ThirdCanvas: types.CanvasModel = {
 				backgroundType: 'img',
 				url: '../images/flowers.png'
 			},
-			filter: 'none',
+			filter: 'red',
 			objects: [
 				{
 					id: 1,
 					position: {
-						x: 300,
-						y: 0 
+						x: 180,
+						y: 150
+					},
+					type: {
+						objectType: 'img',
+						url: '../images/cat.png',
+						size: {
+							width: 400,
+							height: 250
+						}
+					},
+					selectionFlag: false
+				},
+				{
+					id: 2,
+					position: {
+						x: 180,
+						y: 150
+					},
+					type: {
+						objectType: 'figure',
+						type: 'rectangle',
+						size: {
+							width: 400,
+							height: 250
+						},
+						color: "#ffff00"
+					},
+					selectionFlag: false
+				},
+				{
+					id: 3,
+					position: {
+						x: 235,
+						y: 170 
+					},
+					type: {
+						objectType: 'figure',
+						type: 'circle',
+						size: {
+							width: 140,
+							height: 140
+						},
+						color: '#ffffff'
+					},
+					selectionFlag: false
+				},
+				{
+					id: 4,
+					position: {
+						x: 217,
+						y: 90
 					},
 					type: {
 						objectType: 'text',
@@ -132,67 +79,17 @@ const ThirdCanvas: types.CanvasModel = {
 					selectionFlag: false
 				},
 				{
-					id: 2,
-					position: {
-						x: 150,
-						y: 150
-					},
-					type: {
-						objectType: 'img',
-						url: '../images/cat.png',
-						size: {
-							width: 300,
-							height: 200
-						}
-					},
-					selectionFlag: false
-				},
-				{
-					id: 3,
-					position: {
-						x: 200,
-						y: 170 
-					},
-					type: {
-						objectType: 'figure',
-						type: 'circle',
-						size: {
-							width: 100,
-							height: 100
-						},
-						color: '#ffffff'
-					},
-					selectionFlag: false
-				},
-				{
-					id: 4,
-					position: {
-						x: 150,
-						y: 150
-					},
-					type: {
-						objectType: 'figure',
-						type: 'rectangle',
-						size: {
-							width: 300,
-							height: 200
-						},
-						color: "#ffff00"
-					},
-					selectionFlag: false
-				},
-				{
 					id: 5,
 					position: {
-						x: 200,
+						x: 235,
 						y: 50
 					},
 					type: {
 						objectType: 'figure',
 						type: 'triangle',
 						size: {
-							width: 100,
-							height: 150
+							width: 140,
+							height: 200
 						},
 						color: "#00ff00"
 					},
@@ -201,8 +98,8 @@ const ThirdCanvas: types.CanvasModel = {
 				{
 					id: 6,
 					position: {
-						x: 0,
-						y: 0 
+						x: 400,
+						y: 165 
 					},
 					type: {
 						objectType: 'text',
@@ -219,6 +116,5 @@ const ThirdCanvas: types.CanvasModel = {
 };
 
 export {
-	ThirdHistory,
 	ThirdCanvas
 };
